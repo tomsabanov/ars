0f2ba3a972b0e6e2e50df5d6a8dcfc677af8306a
Lucas <lucas@quantum1net.com>
2022-02-07 16:28:28 +0100

    added gradual tuning of a + b,c

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+# /usr/bin/python3
 
 '''
 Assignment 1:
@@ -41,17 +41,17 @@ import matplotlib.animation as animation
 
 
 def rosenbrock(*X):
-    a=0 
-    b=150
-    
+    a = 0
+    b = 150
+
     x = X[0]
     y = X[1]
-    return (a - x)**2 + b*((y-x**2)**2)
+    return (a - x) ** 2 + b * ((y - x ** 2) ** 2)
 
 
 def rastrigin(*X):
     A = 10
-    return A*len(X) + sum([(x**2 - A * np.cos(2 * math.pi * x)) for x in X])
+    return A * len(X) + sum([(x ** 2 - A * np.cos(2 * math.pi * x)) for x in X])
 
 
 class Function:
@@ -66,43 +66,42 @@ class Function:
         # Interval is specified with (min,max)
         intervals = list()
         for x in self.X:
-            intervals.append((min(map(min, x)),max(map(max, x))))
+            intervals.append((min(map(min, x)), max(map(max, x))))
         return intervals
-    
 
-    def plot_function(self,show=True):
+    def plot_function(self, show=True):
         self.fig = plt.figure()
         self.ax = Axes3D(self.fig, azim=-128, elev=43)
 
         # Sidenote: rastrigin looks better without lognorm
         # cmap jet for rastrigin, plasma for rosenbrock with lognorm
-        self.ax.plot_surface(self.X[0], self.X[1], self.value, rstride = 1, cstride = 1, norm = LogNorm(), cmap = cm.jet, linewidth=0, edgecolor='none', alpha=0.5)
+        self.ax.plot_surface(self.X[0], self.X[1], self.value, rstride=1, cstride=1, norm=LogNorm(), cmap=cm.jet,
+                             linewidth=0, edgecolor='none', alpha=0.5)
         plt.xlabel("x")
         plt.ylabel("y")
 
         if show:
             plt.show()
-    
-    def plot_contour(self,show=True):
+
+    def plot_contour(self, show=True):
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111)
 
-        plt.contour(self.X[0], self.X[1], self.value, 300 )
+        plt.contour(self.X[0], self.X[1], self.value, 300)
 
         if show:
             plt.show()
-    
 
 
 class PSO:
-    def __init__(self, 
-                    fnc: Function,
-                    num_of_agents=20,
-                    max_iter = 20,  
-                    a = 1.0, 
-                    b = 2.0, 
-                    c = 2.0
-                ):
+    def __init__(self,
+                 fnc: Function,
+                 num_of_agents=20,
+                 max_iter=1000,
+                 a=0.9,
+                 b=0,
+                 c=0
+                 ):
         self.fnc = fnc
         self.num_of_agents = num_of_agents
         self.max_iter = max_iter
@@ -130,8 +129,7 @@ class PSO:
         # Evaluations of best positions
         self.best_evaluations = np.zeros(num_of_agents)
 
-
-        # Save all of the positions in a history (list of lists, where each list 
+        # Save all of the positions in a history (list of lists, where each list
         # has all of the positions of a single particle)
         self.hist = list()
         self.prepare_history()
@@ -143,7 +141,7 @@ class PSO:
     def generate_initial_velocities(self):
         velocities = np.zeros((self.num_of_agents, self.fnc.dim))
         for i in range(len(velocities)):
-            velocities[i] = (rnd.uniform(-1,1), rnd.uniform(-1,1))
+            velocities[i] = (rnd.uniform(-1, 1), rnd.uniform(-1, 1))
         return velocities
 
     def generate_initial_positions(self):
@@ -152,16 +150,14 @@ class PSO:
 
         # Get intervals on which the function is specified
         intervals = self.fnc.get_dimension_intervals()
-        
+
         # Iterate over each position and each interval and generate a random val in that interval and update the value in positions
         for i in range(len(positions)):
             for j in range(len(intervals)):
                 rand_val = rnd.uniform(intervals[j][0], intervals[j][1])
                 positions[i][j] = rand_val
-        
-        return positions
-    
 
+        return positions
 
     def evaluate_particles(self):
         for i in range(self.num_of_agents):
@@ -169,7 +165,7 @@ class PSO:
             eval = self.fnc.call(*pos)
 
             # Add to history
-            self.hist[i].append((pos[0],pos[1],eval))
+            self.hist[i].append((pos[0], pos[1], eval))
 
             pos_best = self.best_positions[i]
             best_eval = self.fnc.call(*pos_best)
@@ -178,13 +174,12 @@ class PSO:
             if eval < best_eval:
                 self.best_positions[i] = pos
                 self.best_evaluations[i] = eval
-            
+
             # check if particle best position is best globally and update index
             if best_eval < self.fnc.call(*self.best_positions[self.best_global_position]):
                 self.best_global_position = i
 
             self.evaluations[i] = eval
-    
 
     def update_velocities(self):
         for i in range(self.num_of_agents):
@@ -206,7 +201,7 @@ class PSO:
 
         for i in range(self.num_of_agents):
             pos = self.positions[i]
-            new_pos = pos  + self.velocities[i]
+            new_pos = pos + self.velocities[i]
             self.positions[i] = new_pos
 
             # Check if position is out of defined bounds
@@ -216,9 +211,10 @@ class PSO:
 
                 if self.positions[i][j] > intervals[j][1]:
                     self.positions[i][j] = intervals[j][1]
+        # print(self.positions[0][0])
 
     def run_simulation(self):
-        
+
         for i in range(self.max_iter):
 
             # 1. Evaluate particle positions
@@ -230,20 +226,29 @@ class PSO:
             # 3. Update new particle positions
             self.update_positions()
 
+            self.a = 0.9 - 0.5 * (i / self.max_iter)
 
+            if i > 800:
+                self.b = 2
+                self.c = 2
 
-    def animation_function(self, num, datasets, dots, graph):        
+            # print(self.a)
+
+        for pos in self.positions:
+            # print(len(pos))
+            print(str(round(pos[0], 2)) + "," + str(round(pos[1], 2)))
+
+    def animation_function(self, num, datasets, dots, graph):
         for i in range(self.num_of_agents):
-            #lines[i].set_data(datasets[i][0:2, :num])    
-            #lines[i].set_3d_properties(datasets[i][2, :num])    
+            # lines[i].set_data(datasets[i][0:2, :num])
+            # lines[i].set_3d_properties(datasets[i][2, :num])
 
-            dots[i].set_data(datasets[i][0:2, :num])    
-            #dots[i].set_3d_properties(datasets[i][2, :num])        
-        
+            dots[i].set_data(datasets[i][0:2, :num])
+            # dots[i].set_3d_properties(datasets[i][2, :num])
+
     def animate(self):
         # We have the figure in the fnc object
         self.fnc.plot_contour(False)
-
 
         # List of lists of x/y/z coordinates where each list holds x/y/z values for a single particle over the entire simulation
         X = list()
@@ -253,47 +258,48 @@ class PSO:
             X.append(list())
             Y.append(list())
             Z.append(list())
-        
+
         for i in range(self.num_of_agents):
             for j in range(self.max_iter):
                 pos = self.hist[i][j]
                 X[i].append(pos[0])
                 Y[i].append(pos[1])
                 Z[i].append(pos[2])
-        
+
         # Create datasets of [x,y,z] for each particle
         datasets = list()
         for i in range(self.num_of_agents):
-            datasets.append([ X[i], Y[i], Z[i] ])
+            datasets.append([X[i], Y[i], Z[i]])
         for i in range(self.num_of_agents):
-            # This has to be done because of some error in animation_function later on 
+            # This has to be done because of some error in animation_function later on
             # when animating
             datasets[i] = np.array(datasets[i])
 
-        
         # Create dots plots and line plots for each dataset
         dots = list()
         for i in range(self.num_of_agents):
-            d = plt.plot(datasets[i][0], datasets[i][1], markersize=10, markeredgecolor="black", markerfacecolor="purple", marker="o",alpha=1.0)[0]
+            d = \
+            plt.plot(datasets[i][0], datasets[i][1], markersize=10, markeredgecolor="black", markerfacecolor="purple",
+                     marker="o", alpha=1.0)[0]
             dots.append(d)
 
-        anim = animation.FuncAnimation(self.fnc.fig, self.animation_function, frames=self.max_iter, fargs=(datasets,dots), interval=1000, blit=False)
+        anim = animation.FuncAnimation(self.fnc.fig, self.animation_function, frames=self.max_iter,
+                                       fargs=(datasets, dots), interval=1000, blit=False)
 
         plt.show()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     # Example rosenbrock
     x = np.arange(-1.5, 2.0, 0.05)
     y = np.arange(-0.5, 3.0, 0.05)
     X, Y = np.meshgrid(x, y)
 
-    rosenbrock_fnc = Function(rosenbrock, X,Y)
+    rosenbrock_fnc = Function(rosenbrock, X, Y)
 
-    pso = PSO(rosenbrock_fnc,2, 5)
+    pso = PSO(rosenbrock_fnc, 20, 1000)
     pso.run_simulation()
     pso.animate()
-    
 
     # Example rastrigin
     # x = np.linspace(-4, 4, 50)    
